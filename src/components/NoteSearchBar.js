cae83d64f0177940bc1d461200809964be5da4fa
Heloise Taillet <heloise96@hotmail.com>
2019-04-10 11:04:50 -0400

    added search bar

---
+++ src/components/NoteSearchBar.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react'
+import { Search, Grid, Header, Segment } from 'semantic-ui-react'
+import _ from 'lodash'
+
+class NoteSearchBar extends Component {
+  componentWillMount() {
+    this.resetComponent()
+  }
+
+  resetComponent = () =>
+    this.setState({ isLoading: false, results: [], value: '' })
+
+  handleResultSelect = (e, { result }) => this.props.onSearchSelect(result)
+
+  handleSearchChange = (e, { value }) => {
+    this.setState({ isLoading: true, value })
+
+    setTimeout(() => {
+      if (this.state.value.length < 1) return this.resetComponent()
+
+      const re = new RegExp(_.escapeRegExp(this.state.value), 'i')
+      const isMatch = result => re.test(result.title) || re.test(result.description)
+
+      this.setState({
+        isLoading: false,
+        results: _.filter(this.props.notes, isMatch),
+      })
+    }, 300)
+  }
+
+  render() {
+    const { isLoading, value, results } = this.state
+
+    return (
+      <Grid>
+        <Grid.Column width={6}>
+          <Search
+            input={{ icon: 'search', iconPosition: 'left' }}
+            loading={isLoading}
+            onResultSelect={this.handleResultSelect}
+            onSearchChange={_.debounce(this.handleSearchChange, 500, {
+              leading: true,
+            })}
+            results={results}
+            value={value}
+            {...this.props}
+          />
+        </Grid.Column>
+      </Grid>
+    )
+  }
+}
+
+export default NoteSearchBar
--- src/containers/NotesContainer.js
+++ src/containers/NotesContainer.js
@@ -4,18 +4,27 @@ import { createStore, combineReducers, applyMiddleware, compose, bindActionCreat
 import { Card } from 'semantic-ui-react'
 import { fetchingNotes } from '../redux/actions'
 import NoteCard from '../components/NoteCard'
+import NoteSearchBar from '../components/NoteSearchBar'
+
 class NotesContainer extends React.Component {
   componentDidMount() {
     this.props.fetchingNotes()
   }
 
+  onSearchSelect = (result) => console.log("result from notes container", result)
+
   render() {
     return (
+      <>
+      <div className="flex-container">
+      <NoteSearchBar className="row" notes={this.props.notes} onSearchSelect={this.onSearchSelect}/>
+      </div>
       <div className="flex-container">
       <Card.Group>
       {this.props.notes.map(note=> <NoteCard note={note}/>)}
       </Card.Group>
       </div>
+      </>
     )
   }
 }
